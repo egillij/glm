fe0145c8cbc3573d7c5077dda7afd989bafdb6e1
= <=>
2020-12-30 18:42:58 +0100

    premake build file

---
+++ premake5.lua
@@ -0,0 +1,24 @@
+project "glm"
+	kind "StaticLib"
+	language "C"
+	
+	targetdir("bin/" .. outputdir .. "/%{prj.name}")
+	objdir("bin-int/" .. outputdir .. "/%{prj.name}")
+	
+	files
+	{
+		"glm/**.hpp",
+		"glm/**.inl"
+	}
+	
+	filter "system:windows"
+		systemversion "latest"
+		staticruntime "On"
+		
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
